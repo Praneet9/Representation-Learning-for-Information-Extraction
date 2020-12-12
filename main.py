8bad246d157e8918f13fb856929a7e63503afbff
mukul <mukul.kumar@blackstraw.ai>
2020-12-13 01:11:58 +0530

    added candidate to ingestion pipeline

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from pathlib import Path
-from utils import xml_parser,Neighbour, visualizer
+from utils import xml_parser, Neighbour, visualizer, candidate
 import cv2
 
 this_dir = Path.cwd()
 xmls_path = this_dir / "dataset" / "xmls"
 ocr_path = this_dir / "dataset" / "tesseract_results"
 image_path = this_dir / "dataset" / "images"
+candidate_path = this_dir / "dataset" / "candidates"
 
 annotation, classes_count, class_mapping = xml_parser.get_data(xmls_path)
+annotation = candidate.attach_candidate(annotation, candidate_path)
 
 images = list(image_path.glob("*.jpg"))
-
 # for img in images:
 #     out_img = visualizer.tesseract_ocr(img, ocr_path / (img.stem + ".json"))
 #     cv2.imwrite('test.jpg', out_img)
--- utils/xml_parser.py
+++ utils/xml_parser.py
@@ -23,19 +23,19 @@ def get_data(xml_path):
                 continue
 
             annotation_data = {'filename': annot.stem, 'width': element_width,
-                               'height': element_height, 'field': {'invoice_no': {'true_candidate': [],
-                                                                                  'other_candidate': []},
-                                                                   'invoice_date': {'true_candidate': [],
-                                                                                    'other_candidate': []},
-                                                                   'total': {'true_candidate': [],
-                                                                             'other_candidate': []}}}
-            for i, cls in enumerate(annotation_data['field']):
+                               'height': element_height, 'fields': {'invoice_no': {'true_candidates': [],
+                                                                                   'other_candidates': []},
+                                                                    'invoice_date': {'true_candidates': [],
+                                                                                     'other_candidates': []},
+                                                                    'total': {'true_candidates': [],
+                                                                              'other_candidates': []}}}
+            for i, cls in enumerate(annotation_data['fields']):
                 classes_count[cls] = 0
                 class_mapping[cls] = i
 
             for element_obj in element_objs:
                 class_name = element_obj.find('name').text
-                if class_name not in annotation_data['field']:
+                if class_name not in annotation_data['fields']:
                     print("Unidentified field Found:", class_name, "in file:", annot.name)
                     continue
                 else:
@@ -47,8 +47,8 @@ def get_data(xml_path):
                 x2 = int(round(float(obj_bbox.find('xmax').text)))
                 y2 = int(round(float(obj_bbox.find('ymax').text)))
                 difficulty = int(element_obj.find('difficult').text) == 1
-                annotation_data['field'][class_name]['true_candidate'].append({'bbox': {'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2},
-                                                             'difficult': difficulty})
+                annotation_data['fields'][class_name]['true_candidates'].append({'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2,
+                                                                                 'difficult': difficulty})
 
             annotations.append(annotation_data)
 
