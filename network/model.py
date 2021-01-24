61d32ca2756c98cf1025fb28658590992cd31220
Praneet9 <praneetbomma@gmail.com>
2021-01-24 17:47:29 +0530

    complete architecture

--- network/model.py
+++ network/model.py
@@ -2,42 +2,57 @@
 
 import torch
 from torch import nn
+import torch.nn.functional as F
 
-from network import embedding
+from network.neighbour_attention import MultiHeadAttention
+from network.neighbour_embedding import NeighbourEmbedding
 
-class ReLIE(nn.Module):
-    
-    def __init__(self, vocab_size, embedding_dim, n_neighbours):
+
+class Model(nn.Module):
+
+    def __init__(self, vocab_size, embedding_dim, neighbours, heads):
         super().__init__()
-        
-        # Field Embedding (Probably One hot encoded data)
-        
-        # Candidate Embedding Layer
-        # One dense layer
-        
-        # Neighbour Word Embedding and Positional Embedding
-        # Word Embedding - Embedding layer
-        # Positional Embedding - 2 blocks of (Dense, Relu, Dropout, Dense, Relu, Dropout)
-        # N number of Neighbours
-        
-        # Each neighbour goes through a Neighbour Encoding Attention layer of its own
-        
-        # All the neighbour encodings are concatenated together into one block
-        # This concatenated encoding is concatenated with Candidate Positon Encoding for Candidate Encoding
-        
-        # This candidate encoding is merged with Field Embedding and produces a binary output score
-        
+
         self.cand_embed = nn.Linear(2, 128)
-        self.field_embed = nn.Linear(2, 128)
-        
-        self.neighbour_embeddings = embedding.Embedder(vocab_size, embedding_dim, n_neighbours)
-        
-        
-    
-    def forward(self, x):
-        neighbour_words = x.view([-1,6,3])[:,1:,:1].type(torch.LongTensor).view(-1, 5)
-        neighbour_cords = x.view([-1,6,3])[:,1:,1:]
-        
-        embeds = self.neighbour_embeddings(neighbour_words, neighbour_cords)
-        
-        return embeds
+        self.field_embed = nn.Linear(3, embedding_dim)
+        self.embedding_dimension = embedding_dim
+        self.neighbour_embeddings = NeighbourEmbedding(vocab_size, embedding_dim)
+
+        self.attention_encodings = MultiHeadAttention(heads, embedding_dim * 2)
+        self.linear_projection = nn.Linear(neighbours * embedding_dim * 2, 4 * embedding_dim * 2)
+        self.linear_projection_2 = nn.Linear(128 + (2 * embedding_dim), embedding_dim)
+        self.cos_sim = nn.CosineSimilarity(dim=1, eps=1e-6)
+
+    def forward(self, field_id, candidate, neighbour_words, neighbour_positions):
+        # Field and candidate embeddings
+        id_embed = self.field_embed(field_id)
+        cand_embed = self.cand_embed(candidate)
+
+        # Neighbour embeddings
+        neighbour_embeds = self.neighbour_embeddings(neighbour_words, neighbour_positions)
+
+        # Attention encodings
+        self_attention = self.attention_encodings(neighbour_embeds, neighbour_embeds, neighbour_embeds)
+
+        # Linear projection of attention to concatenate with candidate embedding
+        bs = self_attention.size(0)
+        self_attention = self_attention.view(bs, -1)
+        linear_proj = F.relu(self.linear_projection(self_attention))
+
+        linear_proj = linear_proj.view(bs, 4, -1)
+
+        pooled_attention = F.max_pool2d(linear_proj, 2, 2)
+
+        unrolled_attention = pooled_attention.view(bs, -1)
+
+        # Concatenating Candidate embedding and Attention
+        concat = torch.cat((cand_embed, unrolled_attention), dim=1)
+
+        # Re-projecting concatenated embedding to calculate cosing similarity
+        projected_candidate_encoding = F.relu(self.linear_projection_2(concat))
+
+        # Calculating cosine similarity and scaling to [0,1]
+        similarity = self.cos_sim(id_embed, projected_candidate_encoding).view(bs, -1)
+        scores = (similarity + 1) / 2
+
+        return scores
