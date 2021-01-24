635752e3897042d2777908ef9c39f37f1c216f89
mukul <mukul.kumar@blackstraw.ai>
2021-01-24 21:01:48 +0530

    fixed minor issues

--- network/dataset.py
+++ network/dataset.py
@@ -9,21 +9,22 @@ from utils import preprocess
 class DocumentsDataset(data.Dataset):
     """Stores the annotated documents dataset."""
     
-    def __init__(self, xmls_path, ocr_path, image_path, candidate_path,
-                 field_dict, n_neighbour=5, vocab=None):
+    def __init__(self, xmls_path, ocr_path, image_path, candidate_path, n_neighbour=5, vocab=None):
         """ Initialize the dataset with preprocessing """
         annotation, classes_count, class_mapping = xml_parser.get_data(xmls_path)
+        print("Class Mapping:", class_mapping)
+        print("Classs counts:", classes_count)
         annotation = candidate.attach_candidate(annotation, candidate_path)
         annotation = Neighbour.attach_neighbour(annotation, ocr_path)
         annotation = op.normalize_positions(annotation)
-        _data = preprocess.parse_input(annotation, field_dict, n_neighbour, vocab)
+        _data = preprocess.parse_input(annotation, class_mapping, n_neighbour, vocab)
         self.field_ids, self.candidate_cords, self.neighbours, self.neighbour_cords, self.labels, self.vocab = _data
     
     def __len__(self):
         return len(self.field_ids)
     
     def __getitem__(self, idx):
-        
+        print(idx)
         return (
             torch.tensor(self.field_ids[idx]).type(torch.FloatTensor),
             torch.tensor(self.candidate_cords[idx]).type(torch.FloatTensor),
