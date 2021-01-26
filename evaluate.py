8b9641ba1c1aa2aac3fad4d7f9f2df9aeb5608f1
Praneet9 <praneetbomma@gmail.com>
2021-01-26 12:40:30 +0530

    calculating recall score and added focal loss

--- evaluate.py
+++ evaluate.py
@@ -4,6 +4,8 @@ from torch.utils import data
 import utils.constants as constants
 from network.model import Model
 from network import dataset
+from sklearn.metrics import recall_score
+from focal_loss.focal_loss import FocalLoss
 
 
 def evaluate(model, val_dataloader, criterion):
@@ -12,6 +14,8 @@ def evaluate(model, val_dataloader, criterion):
     model = model.to(device)
     val_accuracy = 0.0
     val_loss = 0.0
+    y_preds = []
+    y_labels = []
 
     with torch.no_grad():
         for val_field, val_candidate, val_words, val_positions, val_labels in val_dataloader:
@@ -25,13 +29,17 @@ def evaluate(model, val_dataloader, criterion):
             validation_loss = criterion(val_outputs, val_labels)
 
             val_preds = val_outputs.round()
+            y_preds.extend(list(val_preds.cpu().detach().numpy().reshape(1, -1)[0]))
+            y_labels.extend(list(val_labels.cpu().detach().numpy().reshape(1, -1)[0]))
+
             val_accuracy += torch.sum(val_preds == val_labels).item()
             val_loss += validation_loss.item()
 
         val_loss = val_loss / val_dataloader.sampler.num_samples
         val_accuracy = val_accuracy / val_dataloader.sampler.num_samples
+        recall = recall_score(y_labels, y_preds)
 
-    return val_accuracy, val_loss
+    return val_accuracy, val_loss, recall
 
 
 if __name__ == '__main__':
@@ -43,7 +51,8 @@ if __name__ == '__main__':
     test_data = data.DataLoader(doc_data, batch_size=constants.BATCH_SIZE, shuffle=True)
 
     rlie = Model(VOCAB_SIZE, constants.EMBEDDING_SIZE, constants.NEIGHBOURS, constants.HEADS)
-    criterion = nn.BCELoss()
+    # criterion = nn.BCELoss()
+    criterion = FocalLoss(alpha=2, gamma=5)
 
-    test_accuracy, test_loss = evaluate(rlie, test_data, criterion)
-    print(f"Test Accuracy: {test_accuracy} Test Loss: {test_loss}")
+    test_accuracy, test_loss, test_recall = evaluate(rlie, test_data, criterion)
+    print(f"Test Accuracy: {test_accuracy} Test Loss: {test_loss} Test Recall: {test_recall}")
--- train.py
+++ train.py
@@ -5,24 +5,31 @@ import utils.constants as constants
 from network.model import Model
 from network import dataset
 from evaluate import evaluate
+from sklearn.metrics import recall_score
+from focal_loss.focal_loss import FocalLoss
 
 
 def train(model, train_dataloader, val_dataloader, epochs):
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
-    criterion = nn.BCELoss()
+    # criterion = nn.BCELoss()
+    criterion = FocalLoss(alpha=2, gamma=5)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     train_loss_history = []
     train_accuracy_history = []
+    recall_history = []
     val_loss_history = []
     val_accuracy_history = []
+    val_recall_history = []
 
     for epoch in range(1, epochs + 1):
 
         train_loss = 0.0
         train_accuracy = 0.0
+        y_preds = []
+        y_labels = []
 
         for field, candidate, words, positions, labels in train_dataloader:
 
@@ -40,31 +47,39 @@ def train(model, train_dataloader, val_dataloader, epochs):
             optimizer.step()
 
             preds = outputs.round()
+            y_preds.extend(list(preds.cpu().detach().numpy().reshape(1, -1)[0]))
+            y_labels.extend(list(labels.cpu().detach().numpy().reshape(1, -1)[0]))
+
             train_accuracy += torch.sum(preds == labels).item()
             train_loss += loss.item()
 
         else:
 
-            val_accuracy, val_loss = evaluate(model, val_dataloader, criterion)
+            val_accuracy, val_loss, val_recall = evaluate(model, val_dataloader, criterion)
 
             train_loss = train_loss / train_dataloader.sampler.num_samples
             train_accuracy = train_accuracy / train_dataloader.sampler.num_samples
+            recall = recall_score(y_labels, y_preds)
             train_loss_history.append(train_loss)
             train_accuracy_history.append(train_accuracy)
+            recall_history.append(recall)
 
             val_loss_history.append(val_loss)
             val_accuracy_history.append(val_accuracy)
+            val_recall_history.append(val_recall)
 
             print(f"Epoch:{epoch} Loss:{round(train_loss, 4)} \
-                  Accuracy: {round(train_accuracy, 4)} \
-                  Validation Loss: {round(val_loss, 4)} \
-                  Validation Accuracy: {round(val_accuracy, 4)}")
+                    Recall: {round(recall, 4)} \
+                    Validation Loss: {round(val_loss, 4)} \
+                    Validation Recall: {round(val_recall, 4)}")
 
     return {
         'training_loss': train_loss_history,
         'training_accuracy': train_accuracy_history,
+        'training_recall': recall_history,
         'validation_loss': val_loss_history,
-        'validation_accuracy': val_accuracy_history
+        'validation_accuracy': val_accuracy_history,
+        'validation_recall': recall_history
     }
 
 
