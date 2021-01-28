fixed in attention mask

evaluate.py
@@ -18,14 +18,15 @@ def evaluate(model, val_dataloader, criterion):
     y_labels = []
 
     with torch.no_grad():
-        for val_field, val_candidate, val_words, val_positions, val_labels in val_dataloader:
+        for val_field, val_candidate, val_words, val_positions, masks, val_labels in val_dataloader:
             val_field = val_field.to(device)
             val_candidate = val_candidate.to(device)
             val_words = val_words.to(device)
             val_positions = val_positions.to(device)
+            masks = masks.to(device)
             val_labels = val_labels.to(device)
 
-            val_outputs = model(val_field, val_candidate, val_words, val_positions)
+            val_outputs = model(val_field, val_candidate, val_words, val_positions, masks)
             validation_loss = criterion(val_outputs, val_labels)
 
             val_preds = val_outputs.round()

network/model.py
@@ -32,7 +32,7 @@ class Model(nn.Module):
         neighbour_embeds = self.neighbour_embeddings(neighbour_words, neighbour_positions)
 
         # Attention encodings
-        self_attention = self.attention_encodings(neighbour_embeds, neighbour_embeds, neighbour_embeds, mask=None)
+        self_attention = self.attention_encodings(neighbour_embeds, neighbour_embeds, neighbour_embeds, mask=masks)
 
         # Linear projection of attention to concatenate with candidate embedding
         bs = self_attention.size(0)

train.py
@@ -79,13 +79,14 @@ def train(model, train_dataloader, val_dataloader, epochs):
             writer.add_scalar('Loss/validation', val_loss, epoch)
             writer.add_scalar('Accuracy/validation', val_accuracy, epoch)
 
-            print(f"Metrics for Epoch - {epoch}  Loss:{round(train_loss, 4)} \
+            print(f"Metrics for Epoch {epoch}:  Loss:{round(train_loss, 4)} \
                     Recall: {round(recall, 4)} \
                     Validation Loss: {round(val_loss, 4)} \
                     Validation Recall: {round(val_recall, 4)}")
 
     writer.flush()
     writer.close()
+    torch.save(model, 'output/model.pth')  # saving model for inference
     return {
         'training_loss': train_loss_history,
         'training_accuracy': train_accuracy_history,